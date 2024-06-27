83c106d2a238a1aa305f096cd2138a9147037e50
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-27 18:31:06 +0300

    updates

---
+++ JS Syntax Fundamentals - Lab/3.js
@@ -0,0 +1,11 @@
+function checkExcellentGrade(grade) {
+    if (grade >= 5.50) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+// Examples:
+checkExcellentGrade(5.50);  // Output: Excellent
+checkExcellentGrade(4.35);  // Output: Not excellent
