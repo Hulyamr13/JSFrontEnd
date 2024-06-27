bcfcd9ae86958d4788eeb8108fe6e8f9219a5a3c
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-27 18:30:39 +0300

    updates

---
+++ JS Syntax Fundamentals - Lab/10.js
@@ -0,0 +1,21 @@
+function printNumbersFromMtoN(M, N) {
+    for (let i = M; i >= N; i--) {
+        console.log(i);
+    }
+}
+
+// Примери:
+printNumbersFromMtoN(6, 2);
+// Output:
+// 6
+// 5
+// 4
+// 3
+// 2
+
+printNumbersFromMtoN(4, 1);
+// Output:
+// 4
+// 3
+// 2
+// 1
