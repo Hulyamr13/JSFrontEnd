bdd77777c0daf612b74c265d4f97a48bb78c299c
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-27 18:31:26 +0300

    updates

---
+++ JS Syntax Fundamentals - Lab/5.js
@@ -0,0 +1,33 @@
+function mathOperations(num1, num2, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+        default:
+            console.log("Invalid operator");
+            return;
+    }
+
+    console.log(result);
+}
+
+// Examples:
+mathOperations(5, 6, '+');     // Output: 11
+mathOperations(3, 5.5, '*');   // Output: 16.5
