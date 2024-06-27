1cc7e0aba5925c9b36e84cce682920c343f265be
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-27 18:31:56 +0300

    updates

---
+++ JS Syntax Fundamentals - Lab/9.js
@@ -0,0 +1,14 @@
+function printNumbersFrom1to5() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+// Пример:
+printNumbersFrom1to5();
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
