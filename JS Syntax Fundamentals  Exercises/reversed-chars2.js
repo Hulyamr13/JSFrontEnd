7c1f9e4d8b5c5fab07e9e15cfb137fa4ae91480d
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-28 03:30:05 +0300

    updates

---
+++ JS Syntax Fundamentals  Exercises/reversed-chars2.js
@@ -0,0 +1,7 @@
+function reversedChars(char1, char2, char3) {
+    console.log(char3 + ' ' + char2 + ' ' + char1);
+}
+
+
+reversedChars('A', 'B', 'C');  // Изход: C B A
+reversedChars('1', 'L', '&');  // Изход: & L 1
