90bc1a9ee9cf41c935bbdfa793ce39f1aa524a94
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-06-28 03:29:56 +0300

    updates

---
+++ JS Syntax Fundamentals  Exercises/char-to-str.js
@@ -0,0 +1,9 @@
+function charsToString(char1, char2, char3) {
+    let result = char1 + char2 + char3;
+    console.log(result);
+}
+
+
+charsToString('a', 'b', 'c');  // Изход: abc
+charsToString('%', '2', 'o');  // Изход: %2o
+charsToString('1', '5', 'p');  // Изход: 15p
