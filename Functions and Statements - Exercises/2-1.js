89472bed14eac57867ea6cfc4b43d88b8b62b4cb
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-01 01:02:51 +0300

    updates

---
+++ Functions and Statements - Exercises/2-1.js
@@ -0,0 +1,9 @@
+function sumAndSubtract(num1, num2, num3) {
+    let result = (num1 + num2) - num3;
+    console.log(result);
+}
+
+// Examples
+sumAndSubtract(23, 6, 10);   // Output: 19
+sumAndSubtract(1, 17, 30);   // Output: -12
+sumAndSubtract(42, 58, 100); // Output: 0
