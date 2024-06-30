f050d677e42ddc2334ad8d4bbf68d60564039058
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-01 00:56:55 +0300

    updates

---
+++ Functions and Statements - Lab/3.js
@@ -0,0 +1,7 @@
+function repeatString(str, n) {
+    return str.repeat(n);
+}
+
+
+console.log(repeatString("abc", 3));    // Output: "abcabcabc"
+console.log(repeatString("String", 2)); // Output: "StringString"
