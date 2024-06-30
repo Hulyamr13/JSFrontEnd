10fd656e95bcbbf763ce8cff3c3ccc21f65e119b
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-01 01:02:59 +0300

    updates

---
+++ Functions and Statements - Exercises/1.js
@@ -0,0 +1,10 @@
+function smallestNumber(num1, num2, num3) {
+    let smallest = Math.min(num1, num2, num3);
+    console.log(smallest);
+}
+
+// Examples
+smallestNumber(2, 5, 3);    // Output: 2
+smallestNumber(600, 342, 123);  // Output: 123
+smallestNumber(25, 21, 4);   // Output: 4
+smallestNumber(2, 2, 2);     // Output: 2
