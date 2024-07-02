29bc3bd475f4e63cde1094b0366dcc4b0db6c13c
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-02 23:06:25 +0300

    updates

---
+++ Objects and Classes - Lab/7.js
@@ -0,0 +1,38 @@
+function addressBook(input) {
+    let addressBook = {};
+
+    input.forEach(entry => {
+        let [name, address] = entry.split(':');
+        addressBook[name] = address;
+    });
+
+    let sortedNames = Object.keys(addressBook).sort();
+
+    sortedNames.forEach(name => {
+        console.log(`${name} -> ${addressBook[name]}`);
+    });
+}
+
+// Test cases
+addressBook([
+    'Tim:Doe Crossing',
+    'Bill:Nelson Place',
+    'Peter:Carlyle Ave',
+    'Bill:Ornery Rd'
+]);
+
+console.log();
+
+addressBook([
+    'Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd'
+]);
