905ec1c571b362b962b6c994b6e0c46b7199ebe2
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-02 23:06:29 +0300

    updates

---
+++ Objects and Classes - Lab/8.js
@@ -0,0 +1,29 @@
+function catConstructor(cats) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (let cat of cats) {
+        let [catName, catAge] = cat.split(' ');
+        let catFromClass = new Cat(catName, catAge);
+        catFromClass.meow();
+    }
+}
+
+catConstructor(['Mellow 2', 'Tom 5']);
+// Output:
+// Mellow, age 2 says Meow
+// Tom, age 5 says Meow
+
+catConstructor(['Candy 1', 'Poppy 3', 'Nyx 2']);
+// Output:
+// Candy, age 1 says Meow
+// Poppy, age 3 says Meow
+// Nyx, age 2 says Meow
