8999a45e7dcc96c649b94dd7eda7d25e634361a0
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-02 23:07:23 +0300

    updates

---
+++ Objects and Classes - Exercises/3.js
@@ -0,0 +1,47 @@
+function storeProvision(currentStock, deliveries) {
+    let stock = {};
+
+    for (let i = 0; i < currentStock.length; i += 2) {
+        let product = currentStock[i];
+        let quantity = Number(currentStock[i + 1]);
+
+        stock[product] = quantity;
+    }
+
+    for (let i = 0; i < deliveries.length; i += 2) {
+        let product = deliveries[i];
+        let quantity = Number(deliveries[i + 1]);
+
+        if (stock.hasOwnProperty(product)) {
+            stock[product] += quantity;
+        } else {
+            stock[product] = quantity;
+        }
+    }
+
+
+    for (let product in stock) {
+        console.log(`${product} -> ${stock[product]}`);
+    }
+}
+
+// Test cases
+storeProvision(
+    [
+        'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
+    ],
+    [
+        'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    ]
+);
+
+console.log();
+
+storeProvision(
+    [
+        'Salt', '2', 'Fanta', '4', 'Apple', '14', 'Water', '4', 'Juice', '5'
+    ],
+    [
+        'Sugar', '44', 'Oil', '12', 'Apple', '7', 'Tomatoes', '7', 'Bananas', '30'
+    ]
+);
