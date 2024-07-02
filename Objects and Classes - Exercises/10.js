894cbce9257c0ced34fe5240229212c475430ff3
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-02 23:07:32 +0300

    updates

---
+++ Objects and Classes - Exercises/10.js
@@ -0,0 +1,29 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power
+        };
+        this.fuel = fuel;
+    }
+
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    }
+}
+
+// Example 1
+let parts1 = { engine: 6, power: 100 };
+let vehicle1 = new Vehicle('a', 'b', parts1, 200);
+vehicle1.drive(100);
+console.log(vehicle1.fuel); // Output: 100
+console.log(vehicle1.parts.quality); // Output: 600
+
+// Example 2
+let parts2 = { engine: 9, power: 500 };
+let vehicle2 = new Vehicle('l', 'k', parts2, 840);
+vehicle2.drive(20);
+console.log(vehicle2.fuel); // Output: 820
