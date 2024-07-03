b3fe7b920702c5b62e3a57af841fa363d10e18a4
Hulya Hugh <122914464+Hulyamr13@users.noreply.github.com>
2024-07-03 21:07:30 +0300

    Update 6.js

--- JS Arrays and Strings-Exercise/6.js
+++ JS Arrays and Strings-Exercise/6.js
@@ -15,6 +15,19 @@ function hashedWords(myString) {
     });
 }
 
+function hashedWords(myString) {
+    const words = myString.split(' ');
+
+    words.forEach(word => {
+        if (word.startsWith('#')) {
+            const cleanWord = word.slice(1); // премахваме '#'
+            if (/^[A-Za-z]+$/.test(cleanWord)) {
+                console.log(cleanWord);
+            }
+        }
+    });
+}
+
 
 
 // Example usage:
