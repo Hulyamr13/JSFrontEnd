a74464c58744d46c00ca44afb3e0110c94f50700
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 01:04:03 +0300

    updates

--- Objects and Classes - Exercises/5.js
+++ Objects and Classes - Exercises/5.js
@@ -24,6 +24,36 @@ function createHeroRegister(input) {
     }
 }
 
+
+function createHeroRegister(input) {
+    let heroes = [];
+
+    for (let line of input) {
+        let [heroName, heroLevelStr, itemsString] = line.split(' / ');
+
+        let heroLevel = Number(heroLevelStr);
+
+        let items = itemsString ? itemsString.split(', ') : [];
+
+        let hero = {
+            name: heroName,
+            level: heroLevel,
+            items: items
+        };
+
+        heroes.push(hero);
+    }
+
+    heroes.sort((a, b) => a.level - b.level);
+
+    heroes.forEach(hero => {
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items.join(', ')}`);
+    });
+}
+
+
 // Test cases
 createHeroRegister([
     'Isacc / 25 / Apple, GravityGun',
