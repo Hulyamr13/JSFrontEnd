3789145707834e8aeac19a9c969a044c96076f87
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 01:03:03 +0300

    updates

--- Objects and Classes - More Exercises/8.js
+++ Objects and Classes - More Exercises/8.js
@@ -28,6 +28,22 @@ function storeCarsInGarages(inputArray) {
 }
 
 
+function storeCarsInGarages(inputArray) {
+    const garages = {};
+
+    inputArray.forEach(input => {
+        const [garageNumber, carInfo] = input.split(' - ');
+        const formattedCarInfo = carInfo.split(', ').map(pair => pair.replace(': ', ' - ')).join(', ');
+        (garages[garageNumber] ||= []).push(formattedCarInfo);
+    });
+
+    for (const [garageNumber, cars] of Object.entries(garages)) {
+        console.log(`Garage № ${garageNumber}`);
+        cars.forEach(car => console.log(`--- ${car}`));
+    }
+}
+
+
 storeCarsInGarages([
     '1 - color: blue, fuel type: diesel',
     '1 - color: red, manufacture: Audi',
