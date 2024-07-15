5d4a7834a269eb80cf02d0edd50eba50c4babc3c
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 01:04:10 +0300

    updates

--- Objects and Classes - Exercises/3.js
+++ Objects and Classes - Exercises/3.js
@@ -25,6 +25,31 @@ function storeProvision(currentStock, deliveries) {
     }
 }
 
+
+function storeProvision(currentStock, deliveries) {
+    let stock = {};
+
+    for (let i = 0; i < currentStock.length; i += 2) {
+        let product = currentStock[i];
+        let quantity = Number(currentStock[i + 1]);
+
+        stock[product] = (stock[product] || 0) + quantity;
+    }
+
+    for (let i = 0; i < deliveries.length; i += 2) {
+        let product = deliveries[i];
+        let quantity = Number(deliveries[i + 1]);
+
+        stock[product] = (stock[product] || 0) + quantity;
+    }
+
+    for (let product in stock) {
+        console.log(`${product} -> ${stock[product]}`);
+    }
+}
+
+
+
 // Test cases
 storeProvision(
     [
