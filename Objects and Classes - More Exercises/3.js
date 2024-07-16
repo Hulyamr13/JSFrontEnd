00f92d20bf0ec1a87282df835d7a10d123e8e9d8
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 21:57:22 +0300

    updates

--- Objects and Classes - More Exercises/3.js
+++ Objects and Classes - More Exercises/3.js
@@ -7,33 +7,6 @@ class Laptop {
 
     turnOn() {
         this.isOn = true;
-        this.quality -= 1;
-    }
-
-    turnOff() {
-        this.isOn = false;
-        this.quality -= 1;
-    }
-
-    showInfo() {
-        return JSON.stringify(this.info);
-    }
-
-    get price() {
-        return 800 - this.info.age * 2 + (this.quality * 0.5);
-    }
-}
-
-
-class Laptop {
-    constructor(info, quality) {
-        this.info = info;
-        this.isOn = false;
-        this.quality = quality;
-    }
-
-    turnOn() {
-        this.isOn = true;
         this._decreaseQuality();
     }
 
