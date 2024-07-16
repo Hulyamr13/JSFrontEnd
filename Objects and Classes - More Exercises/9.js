1c6c32c7a62f7f88db43eb007ce6d6da809905a7
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 21:57:51 +0300

    updates

--- Objects and Classes - More Exercises/9.js
+++ Objects and Classes - More Exercises/9.js
@@ -1,52 +1,6 @@
 function manageArmies(inputArray) {
     const armyLog = {};
 
-    for (const info of inputArray) {
-        if (info.includes('arrives')) {
-            let leader = info.replace(' arrives', '');
-            if (!armyLog.hasOwnProperty(leader)) {
-                armyLog[leader] = {};
-            }
-        } else if (info.includes(':')) {
-            let [leader, armyInfo] = info.split(': ');
-            if (armyLog.hasOwnProperty(leader)) {
-                let [army, num] = armyInfo.split(', ');
-                armyLog[leader][army] = parseInt(num);
-            }
-        } else if (info.includes('+')) {
-            let [army, num] = info.split(' + ');
-            for (let leader of Object.keys(armyLog)) {
-                if (armyLog[leader].hasOwnProperty(army)) {
-                    armyLog[leader][army] += parseInt(num);
-                    break; // No need to continue searching once updated
-                }
-            }
-        } else if (info.includes('defeated')) {
-            let leader = info.replace(' defeated', '');
-            delete armyLog[leader];
-        }
-    }
-
-    const leaderTotals = Object.entries(armyLog).reduce((acc, [leader, armies]) => {
-        acc[leader] = Object.values(armies).reduce((sum, count) => sum + count, 0);
-        return acc;
-    }, {});
-
-    const sortedLeaders = Object.entries(leaderTotals).sort((a, b) => b[1] - a[1]);
-
-    sortedLeaders.forEach(([leader, total]) => {
-        console.log(`${leader}: ${total}`);
-        const armies = Object.entries(armyLog[leader]).sort((a, b) => b[1] - a[1]);
-        armies.forEach(([army, count]) => {
-            console.log(`>>> ${army} - ${count}`);
-        });
-    });
-}
-
-
-function manageArmies(inputArray) {
-    const armyLog = {};
-
     inputArray.forEach(info => {
         if (info.endsWith('arrives')) {
             let leader = info.split(' arrives')[0];
