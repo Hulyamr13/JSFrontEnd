804e8557e97bb20742c8db8c0b9d71df7207eb0e
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 22:02:05 +0300

    updates

--- Objects and Classes - Exercises/3.js
+++ Objects and Classes - Exercises/3.js
@@ -5,34 +5,6 @@ function storeProvision(currentStock, deliveries) {
         let product = currentStock[i];
         let quantity = Number(currentStock[i + 1]);
 
-        stock[product] = quantity;
-    }
-
-    for (let i = 0; i < deliveries.length; i += 2) {
-        let product = deliveries[i];
-        let quantity = Number(deliveries[i + 1]);
-
-        if (stock.hasOwnProperty(product)) {
-            stock[product] += quantity;
-        } else {
-            stock[product] = quantity;
-        }
-    }
-
-
-    for (let product in stock) {
-        console.log(`${product} -> ${stock[product]}`);
-    }
-}
-
-
-function storeProvision(currentStock, deliveries) {
-    let stock = {};
-
-    for (let i = 0; i < currentStock.length; i += 2) {
-        let product = currentStock[i];
-        let quantity = Number(currentStock[i + 1]);
-
         stock[product] = (stock[product] || 0) + quantity;
     }
 
