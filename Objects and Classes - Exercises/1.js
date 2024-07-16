96a7ab5fef22a8aebe4faf9fa870964298423615
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-16 21:56:33 +0300

    updates

--- Objects and Classes - Exercises/1.js
+++ Objects and Classes - Exercises/1.js
@@ -1,20 +1,4 @@
 function printEmployees(inputArray) {
-    let employees = {};
-
-    for (let name of inputArray) {
-        let personalNum = name.length;
-        employees[name] = personalNum;
-    }
-
-    // Output the formatted results
-    for (let name in employees) {
-        let personalNum = employees[name];
-        console.log(`Name: ${name} -- Personal Number: ${personalNum}`);
-    }
-}
-
-
-function printEmployees(inputArray) {
     inputArray.forEach(name => {
         console.log(`Name: ${name} -- Personal Number: ${name.length}`);
     });
