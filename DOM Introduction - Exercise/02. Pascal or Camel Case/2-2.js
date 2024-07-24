29086100ddd704661fddc6d95cd09380cb746115
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-25 00:49:48 +0300

    updates

---
+++ DOM Introduction - Exercise/02. Pascal or Camel Case/2-2.js
@@ -0,0 +1,20 @@
+function solve() {
+    const param1 = document.getElementById('text').value;
+    const param2 = document.getElementById('naming-convention').value;
+
+    const words = param1.split(' ').map(word => word.toLowerCase());
+    let result = '';
+
+    if (param2 === 'Pascal Case' || param2 === 'Camel Case') {
+        result = words.map((word, index) =>
+            index === 0 && param2 === 'Camel Case' ? word : word.charAt(0).toUpperCase() + word.slice(1)
+        ).join('');
+        if (param2 === 'Pascal Case') {
+            result = result.charAt(0).toUpperCase() + result.slice(1);
+        }
+    } else {
+        result = 'Error!';
+    }
+
+    document.getElementById('result').textContent = result;
+}
