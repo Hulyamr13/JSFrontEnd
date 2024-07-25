e7d0a99a81b9cf41132e9157684cca2bb50ba9b4
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-26 00:44:22 +0300

    updates

---
+++ DOM Manipulation and Events - Lab/03. Add-Delete/3-1.js
@@ -0,0 +1,13 @@
+function addItem() {
+    const input = document.querySelector('#newItemText');
+    const newItemText = input.value.trim();
+
+    if (newItemText === '') return;
+
+    const liItem = document.createElement('li');
+    liItem.innerHTML = `${newItemText} <a href="#">[Delete]</a>`;
+    liItem.querySelector('a').addEventListener('click', () => liItem.remove());
+
+    document.querySelector('#items').appendChild(liItem);
+    input.value = '';
+}
