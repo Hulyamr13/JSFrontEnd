24f96e9e44577e8b149b2092c224ad09152812e0
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-26 00:43:51 +0300

    updates

---
+++ DOM Manipulation and Events - Exercise/08. Sudomu/8-1.js
@@ -0,0 +1,36 @@
+function solve() {
+    const sumNumbers = (...nums) => nums.reduce((a, b) => a + b, 0);
+
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    const tableElement = document.querySelector('table');
+    const checkParagraphElement = document.querySelector('#check p');
+    const [quickCheckButton, clearButton] = document.querySelectorAll('tfoot tr td button');
+
+    quickCheckButton.addEventListener('click', () => {
+        const columns = [[], [], []];
+        let correctSum = true;
+
+        rows.forEach(row => {
+            const nums = Array.from(row.querySelectorAll('td input')).map(input => Number(input.value));
+            nums.forEach((num, i) => columns[i].push(num));
+
+            if (sumNumbers(...nums) !== 6) correctSum = false;
+        });
+
+        columns.forEach(column => {
+            if (sumNumbers(...column) !== 6) correctSum = false;
+        });
+
+        tableElement.style.border = correctSum ? '2px solid green' : '2px solid red';
+        checkParagraphElement.textContent = correctSum ? 'You solve it! Congratulations!' : 'NOP! You are not done yet...';
+        checkParagraphElement.style.color = correctSum ? 'green' : 'red';
+    });
+
+    clearButton.addEventListener('click', () => {
+        rows.forEach(row => row.querySelectorAll('td input').forEach(input => input.value = ''));
+
+        tableElement.style.border = '';
+        checkParagraphElement.textContent = '';
+        checkParagraphElement.style.color = '';
+    });
+}
