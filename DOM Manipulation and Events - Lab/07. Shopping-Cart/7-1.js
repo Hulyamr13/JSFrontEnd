f357e87a96215ddca0abcea621297738445727c8
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-26 00:45:25 +0300

    updates

---
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/7-1.js
@@ -0,0 +1,25 @@
+function solve() {
+    const textareaResult = document.querySelector('textarea');
+    const checkoutButton = document.querySelector('.checkout');
+    const allProductsButtons = document.querySelectorAll('.add-product');
+
+    const cartProducts = new Set();
+    let totalMoney = 0;
+
+    allProductsButtons.forEach(button => button.addEventListener('click', () => {
+        const product = button.closest('.product');
+        const productName = product.querySelector('.product-title').textContent;
+        const productPrice = +product.querySelector('.product-line-price').textContent;
+
+        cartProducts.add(productName);
+        totalMoney += productPrice;
+
+        textareaResult.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+    }));
+
+    checkoutButton.addEventListener('click', () => {
+        textareaResult.textContent += `You bought ${[...cartProducts].join(', ')} for ${totalMoney.toFixed(2)}.`;
+
+        document.querySelectorAll('button').forEach(button => button.disabled = true);
+    });
+}
