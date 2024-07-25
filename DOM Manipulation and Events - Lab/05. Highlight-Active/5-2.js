8a4687582566568a57ac98d867bf7e71a6182ef2
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-26 00:44:59 +0300

    updates

---
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/5-2.js
@@ -0,0 +1,13 @@
+function focused() {
+    const inputAreas = document.querySelectorAll('input');
+
+    inputAreas.forEach(input => {
+        input.addEventListener('focus', () => {
+            input.parentNode.classList.add('focused');
+        });
+
+        input.addEventListener('blur', () => {
+            input.parentNode.classList.remove('focused');
+        });
+    });
+}
