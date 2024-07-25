31dbc4d59cceb9b704a9ec557942b2c0dacdccd2
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2024-07-26 00:45:04 +0300

    updates

---
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/6-1.js
@@ -0,0 +1,16 @@
+function validate() {
+    let inputField = document.querySelector('#email');
+    inputField.addEventListener('change', onChangeEvent);
+
+    let regexPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    function onChangeEvent(e) {
+        let currentValue = e.currentTarget.value.trim();
+
+        if (!regexPattern.test(currentValue)) {
+            e.currentTarget.classList.add('error');
+        } else {
+            e.currentTarget.classList.remove('error');
+        }
+    }
+}
