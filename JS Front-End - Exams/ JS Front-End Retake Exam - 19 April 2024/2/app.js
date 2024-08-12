b813c947650db17ce5ae0dabc8992231d0f0053f
Hulya Hugh <122914464+Hulyamr13@users.noreply.github.com>
2024-08-12 23:01:49 +0300

    Update app.js

--- JS Front-End - Exams/ JS Front-End Retake Exam - 19 April 2024/2/app.js
+++ JS Front-End - Exams/ JS Front-End Retake Exam - 19 April 2024/2/app.js
@@ -1,87 +1,59 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    try {
-        let formElement = document.querySelector("form");
-        let ageElement = document.getElementById("age");
-        let typeElement = document.getElementById("type");
-        let genderElement = document.getElementById("gender");
+  adoptMe = {
+    inputFields: [
+      document.querySelector("#type"),
+      document.querySelector("#gender"),
+      document.querySelector("#age"),
+    ],
+    form: document.querySelector("form"),
+    adoptBtn: document.querySelector("#adopt-btn"),
+    adoptionInfo: document.querySelector("#adoption-info"),
+    adoptedList: document.querySelector("#adopted-list"),
+    inputData: [],
+  };
+  const isEmptySpace = (inputs) => inputs.some((x) => x.value === "");
 
-        let addButton = document.getElementById("adopt-btn");
-        addButton.addEventListener("click", onAdd);
+  const editFunc = (e) => {
+    const currentElement = e.target.parentElement.parentElement;
+    adoptMe.adoptionInfo.removeChild(currentElement);
+    adoptMe.inputData = [...currentElement.querySelectorAll("p")].map(
+      (x) => x.textContent.split(":")[1]
+    );
+    adoptMe.inputFields.forEach((x, i) => (x.value = adoptMe.inputData[i]));
+  };
 
-        function onAdd(e) {
-            e.preventDefault();
-            if (ageElement.value == "" || typeElement.value == "" || genderElement.value == "") {
-                throw new Error("All fields must be filled out.");
-            }
-            let taskList = document.getElementById("adoption-info");
-            let doneList = document.getElementById("adopted-list");
-
-            let taskLiElement = document.createElement("li");
-            let buttonDiv = document.createElement("div");
-            buttonDiv.setAttribute("class", "buttons");
-            let taskArticleElement = document.createElement("article");
-
-            let ageP = document.createElement("p");
-            ageP.textContent = `Age:${ageElement.value}`;
-            let name = ageElement.value;
-
-            let petP = document.createElement("p");
-            petP.textContent = `Pet:${typeElement.value}`;
-            let phone = typeElement.value;
-
-            let genderP = document.createElement("p");
-            genderP.textContent = `Gender:${genderElement.value}`;
-            let gender = genderElement.value;
-
-            taskArticleElement.appendChild(petP);
-            taskArticleElement.appendChild(genderP);
-            taskArticleElement.appendChild(ageP);
-
-            let editBtn = document.createElement("button");
-            editBtn.setAttribute("class", "edit-btn");
-            editBtn.addEventListener("click", edit);
-            editBtn.textContent = "Edit";
-
-            let doneBtn = document.createElement("button");
-            doneBtn.setAttribute("class", "done-btn");
-            doneBtn.addEventListener("click", onDone);
-            doneBtn.textContent = "Done";
-
-            buttonDiv.appendChild(editBtn);
-            buttonDiv.appendChild(doneBtn);
-
-            taskLiElement.appendChild(taskArticleElement);
-            taskLiElement.appendChild(buttonDiv);
-
-            taskList.appendChild(taskLiElement);
-            formElement.reset();
-
-            function edit() {
-                ageElement.value = name;
-                typeElement.value = phone;
-                genderElement.value = gender;
-                taskList.removeChild(taskLiElement);
-            }
-
-            function onDone() {
-                let taskDoneLiElement = document.createElement("li");
-                let deleteBtn = document.createElement("button");
-                deleteBtn.setAttribute("class", "clear-btn");
-                deleteBtn.addEventListener("click", onDelete);
-                deleteBtn.textContent = "Clear";
-                taskDoneLiElement.appendChild(taskArticleElement);
-                taskDoneLiElement.appendChild(deleteBtn);
-                doneList.appendChild(taskDoneLiElement);
-                taskList.removeChild(taskLiElement);
-
-                function onDelete() {
-                    doneList.removeChild(taskDoneLiElement);
-                }
-            }
-        }
-    } catch (error) {
-        console.error("An error occurred:", error.message);
-    }
+  const doneFunc = (e) => {
+    const currentElement = e.target.parentElement.parentElement;
+    adoptMe.adoptionInfo.removeChild(currentElement);
+    currentElement.querySelector(".buttons").remove();
+    currentElement.innerHTML += `<button class="clear-btn">Clear</button>`;
+    currentElement.querySelector("button").addEventListener("click", (e) => {
+      e.target.parentElement.remove();
+    });
+    adoptMe.adoptedList.appendChild(currentElement);
+  };
+  const adoptFunc = (e) => {
+    e.preventDefault();
+    if (isEmptySpace(adoptMe.inputFields)) return;
+    console.log(adoptMe.inputFields.map((x) => x.value));
+    adoptMe.adoptionInfo.innerHTML += `
+            <li>
+              <article>
+              ${["Pet:", "Gender:", "Age:"]
+                .map((x, i) => `<p>${x}${adoptMe.inputFields[i].value}</p>`)
+                .join("")}
+              </article>
+              <div class="buttons">
+                <button class="edit-btn">Edit</button>
+                <button class="done-btn">Done</button>
+              </div>
+            </li>`;
+    [...adoptMe.adoptionInfo.querySelectorAll("button")].forEach((x, i) => {
+      x.addEventListener("click", i % 2 === 0 ? editFunc : doneFunc);
+    });
+    adoptMe.form.reset();
+  };
+  adoptMe.adoptBtn.addEventListener("click", adoptFunc);
 }
